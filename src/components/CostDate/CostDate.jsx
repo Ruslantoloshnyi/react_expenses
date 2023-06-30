75044e89ed40807a6f6f08e32bc3dd5bc3aae1e0
Ruslan <ru@skylinehealthgroup.com>
2023-06-30 15:15:23 +0300

    add costdate component and costdate.css

---
+++ src/components/CostDate/CostDate.jsx
@@ -0,0 +1,13 @@
+import "./CostDate.css";
+
+function CostDate() {
+  return (
+    <div className="cost-date">
+      <div className="cost-date__mounth">mounth</div>
+      <div className="cost-date__year">year</div>
+      <div className="cost-date__day">day</div>
+    </div>
+  );
+}
+
+export default CostDate;
--- src/components/CostItem/CostItem.jsx
+++ src/components/CostItem/CostItem.jsx
@@ -1,9 +1,10 @@
 import "./CostItem.css";
+import CostDate from "../CostDate/CostDate";
 
 function CostItem() {
   return (
     <div className="cost-item">
-      <div>March 2021 12</div>
+      <CostDate></CostDate>
       <div className="cost-item__description">
         <h2>Refrigerator</h2>
         <div className="cost-item__price">1000$</div>
